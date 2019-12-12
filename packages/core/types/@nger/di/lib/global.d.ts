f0d6b68ef16392ab4921c140962853ecbd4c1bf6
imeepos <1037483576@qq.com>
2019-12-12 23:14:55 +0800

    delete types

--- packages/core/types/@nger/di/lib/global.d.ts
+++
@@ -1,7 +0,0 @@
-declare const _global: any;
-/**
- * Attention: whenever providing a new value, be sure to add an
- * entry into the corresponding `....externs.js` file,
- * so that closure won't use that global for its purposes.
- */
-export { _global as global };
--- packages/core/types/@nger/di/lib/providerToStaticProvicer.d.ts
+++
@@ -1,4 +0,0 @@
-import { StaticProvider, Provider } from "./type";
-import { INgerDecorator } from '@nger/decorator';
-export declare function providerToStaticProvider(provider: Provider): StaticProvider;
-export declare function getClassInjectDeps(nger: INgerDecorator): any[];
